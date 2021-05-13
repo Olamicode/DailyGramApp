43c8b87a4ee875d575701eea5546294518b6730d
Olamicode <oafagbemi@gmail.com>
2021-05-13 19:05:55 +0100

    top news fragment setup done

--- app/src/main/java/com/olamachia/dailygramapp/features/bookmarks/BookmarksFragment.kt
+++ app/src/main/java/com/olamachia/dailygramapp/features/bookmarks/BookmarksFragment.kt
@@ -1,12 +1,6 @@
 package com.olamachia.dailygramapp.features.bookmarks
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.olamachia.dailygramapp.R
 
-class BookmarksFragment : Fragment(R.layout.fragment_bookmarks) {
-
-}
+class BookmarksFragment : Fragment(R.layout.fragment_bookmarks)
--- app/src/main/java/com/olamachia/dailygramapp/utils/Helpers.kt
+++ app/src/main/java/com/olamachia/dailygramapp/utils/Helpers.kt
@@ -1,5 +1,1 @@
 package com.olamachia.dailygramapp.utils
-
-import android.app.Activity
-import androidx.fragment.app.Fragment
-
